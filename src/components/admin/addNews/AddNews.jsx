8aeca7e62bbbe4e69d7293d2cae736b93059b718
Abhinav Sinha <abhinavsinha135@gmail.com>
2023-12-05 15:44:08 +0530

    fetch data from firebase done

--- src/components/admin/addNews/AddNews.jsx
+++ src/components/admin/addNews/AddNews.jsx
@@ -1,6 +1,175 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import styles from "./AddNews.module.css";
+import Card from "../../card/Card";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { db, storage } from "../../../firebase/config";
+import { toast } from "react-toastify";
+import { Timestamp, addDoc, collection } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import Loader from "../../loader/Loader";
+
+const categories = [
+  { id: 1, name: "Computer Science and Engineering" },
+  { id: 2, name: "Information Technology" },
+  { id: 3, name: "Electronics and Telecommunication" },
+  { id: 4, name: "Civil Engineering" },
+  { id: 5, name: "Mechanical Engineering" },
+];
+
+const initialState = {
+  title: "",
+  imageURL: "",
+  branch: "",
+  desc: "",
+};
+
 const AddNews = () => {
+  const [news, setNews] = useState({
+    ...initialState
+  });
+
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNews({ ...news, [name]: value });
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    // console.log(file);
+    const storageRef = ref(storage, `collegeWebsite/${Date.now()}${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(progress);
+      },
+      (error) => {
+        toast.error(error.message);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setNews({ ...news, imageURL: downloadURL });
+          toast.success("Image uploaded successfully");
+        });
+      }
+    );
+  };
+
+  const addNews = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const docRef = addDoc(collection(db, "news"), {
+        title: news.title,
+        imageURL: news.imageURL,
+        branch: news.branch,
+        desc: news.desc,
+        createdAt: Timestamp.now().toDate(),
+      });
+      setIsLoading(false);
+      setUploadProgress(0);
+      setNews({...initialState});
+      toast.success("Product uploaded successfully.")
+      navigate("/admin/all-news");
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <div>AddNews</div>
-  )
-}
-export default AddNews
+    <>
+    {isLoading && <Loader />}
+      <div className={styles.news}>
+        <h1>Add New News</h1>
+        <Card cardClass={styles.card}>
+          <form onSubmit={addNews}>
+            <label>News Title:</label>
+            <input
+              type="text"
+              placeholder="News Title"
+              required
+              name="title"
+              value={news.title}
+              onChange={(e) => handleInputChange(e)}
+            />
+
+            <label>Notice Image:</label>
+            <Card cardClass={styles.group}>
+              {uploadProgress === 0 ? null : (
+                <div className={styles.progress}>
+                  <div
+                    className={styles["progress-bar"]}
+                    style={{ width: `${uploadProgress}%` }}
+                  >
+                    {uploadProgress < 100
+                      ? `Uploading ${uploadProgress}%`
+                      : `Upload Complete ${uploadProgress}%`}
+                  </div>
+                </div>
+              )}
+              <input
+                type="file"
+                accept="image/*"
+                placeholder="News Image"
+                name="image"
+                onChange={(e) => handleImageChange(e)}
+              />
+
+              {news.imageURL === "" ? null : (
+                <input
+                  type="text"
+                  // required
+                  placeholder="Image URL"
+                  name="imageURL"
+                  disabled
+                  value={news.imageURL}
+                />
+              )}
+            </Card>
+
+            <label>Branch:</label>
+            <select
+              required
+              name="branch"
+              value={news.branch}
+              onChange={(e) => handleInputChange(e)}
+            >
+              <option value="" disabled>
+                -- Choose Branch --
+              </option>
+              {categories.map((cat) => {
+                return (
+                  <option key={cat.id} value={cat.name}>
+                    {cat.name}
+                  </option>
+                );
+              })}
+            </select>
+
+            <label>Description</label>
+            <textarea
+              name="desc"
+              required
+              value={news.desc}
+              onChange={(e) => handleInputChange(e)}
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button className="--btn --btn-primary">Upload News</button>
+          </form>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default AddNews;
--- src/components/admin/viewNews/ViewNews.jsx
+++ src/components/admin/viewNews/ViewNews.jsx
@@ -1,6 +1,82 @@
+import { useEffect, useState } from "react";
+import styles from "./ViewNews.module.css";
+import { toast } from "react-toastify";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../../../firebase/config";
+import { Link } from "react-router-dom";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+
 const ViewNews = () => {
+  const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  const getNews = () => {
+    setIsLoading(true);
+    try {
+      const newsRef = collection(db, "news");
+      const q = query(newsRef, orderBy("createdAt", "desc"));
+      onSnapshot(q, (snapshot) => {
+        // console.log(snapshot.docs);
+        const allNews = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        // console.log(allNews);
+        setNews(allNews);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <div>ViewNews</div>
-  )
-}
-export default ViewNews
+    <>
+      <div className={styles.table}>
+        <h2>View News</h2>
+        {news.length === 0 ? (
+          <p>No news found.</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>S.No</th>
+                <th>Title</th>
+                <th>Branch</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {news.map((news1, index) => {
+                const { id, title, branch } = news1;
+                return (
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td>{title}</td>
+                    <td>{branch}</td>
+                    <td className={styles.icons}>
+                      <Link to={`/admin/add-news/${id}`}>
+                        <FaEdit size={20} color="green" />
+                      </Link>
+                      &nbsp;
+                      <FaTrashAlt
+                        size={18}
+                        color="red"
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  );
+};
+export default ViewNews;
